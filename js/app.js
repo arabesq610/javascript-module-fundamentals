46049d5cec36eec2a1cf780832111a4a0aad6bc4
Elizabeth Linn <elise_81@yahoo.com>
2016-11-26 18:27:52 -0800

    with webpack

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 define(['./player', './game'], function (player, game) {
 
-    console.log('Loading with RequireJS');
+    console.log('Bundled with Webpack');
 
     // add click handler to the start game button
     document.getElementById('startGame').addEventListener('click', function () {
